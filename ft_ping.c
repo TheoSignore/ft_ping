af65fd54a347e3489e9bea41750842eb8a4891ab
Theo SIGNORE <tsignore@paul-f4Ar4s4.clusters.42paris.fr>
2023-07-07 11:42:49 +0200

    04 - working on reply data extraction

--- ft_ping.c
+++ ft_ping.c
@@ -58,6 +58,7 @@ int	main(int ac, char** av)
 		printf("I need something to ping\n");
 		return (1);
 	}
+	printf ("PID: %i\n", getpid());
 
 	struct sockaddr_in	lipton;
 	zerocalcare(&lipton, sizeof(lipton));
@@ -65,17 +66,25 @@ int	main(int ac, char** av)
 	if (ret)
 		return (1);
 
-	int	suck = socket(AF_INET, SOCK_DGRAM, 1);
+	int	suck = socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP);
 	if (suck == -1)
 	{
 		perror("socket");
+		return (1);
+	}
+	ret = 1;
+	ret = setsockopt(suck, IPPROTO_IP, IP_RECVTTL, &ret, sizeof(ret));
+	if (ret != 0)
+	{
+		perror("setsockoption");
 		return (1);
 	}
 
 	struct icmphdr icmp_hdr;
 	zerocalcare(&icmp_hdr, sizeof(icmp_hdr));
 	icmp_hdr.type = ICMP_ECHO;
-	icmp_hdr.un.echo.id = 42;
+	icmp_hdr.un.echo.id = getpid();
+	icmp_hdr.un.echo.sequence = 1;
 
 	char*	text = "Lorem ipsum dolor sit amet, consectetur adipiscing odio.";
 	size_t	textsize = ft_strlen(text);
@@ -103,14 +112,65 @@ int	main(int ac, char** av)
 
 	msg_hdr.msg_iov = NULL;
 
-	char	msgname[1024];
-	zerocalcare(msgname, 1024);
+	char	msgname[40];
+	zerocalcare(msgname, 40);
 	msg_hdr.msg_name = msgname;
-	msg_hdr.msg_namelen = 1024;
+	msg_hdr.msg_namelen = 40;
 
-	size_t	rcvd = recvmsg(suck, &msg_hdr, 0);
+	char	msgctrl[1024];
+	zerocalcare(msgctrl, 1024);
+	msg_hdr.msg_control = msgctrl;
+	msg_hdr.msg_controllen = 1024;
+
+	struct iovec	shit[1];
+	shit[0].iov_len = 64;
+	shit[0].iov_base = malloc(64);
+
+	msg_hdr.msg_iov = shit;
+	msg_hdr.msg_iovlen = 1;
+
+
+	int	rcvd = recvmsg(suck, &msg_hdr, 0);
+	if (rcvd == -1)
+		return (1);
 	(void)rcvd;
 	printf("%s - %s\n", inet_ntoa(lipton.sin_addr), inet_ntoa(((struct sockaddr_in*)msg_hdr.msg_name)->sin_addr));
 
+	struct icmphdr	icmprpl;
+	char			buffer[57];
+	zerocalcare(buffer, 57);
+	mmcpy(shit[0].iov_base, &icmprpl, sizeof(struct icmphdr));
+	mmcpy(shit[0].iov_base + sizeof(struct icmphdr), &buffer, 56);
+
+	if (icmprpl.type == ICMP_ECHOREPLY)
+		printf("%i: %s\n",icmprpl.un.echo.sequence, buffer);
+	else
+		printf("FUCK\n");
+
 	close(suck);
 }
+
+typedef struct s_icmpecho
+{
+	size_t			len;
+	unsigned char	type;
+	char*			fqdn;
+	char			ip[33];
+	unsigned int	icmp_seq;
+	char*			data;
+	size_t			datalen;
+}	icmpecho_t;
+
+int	get_ttl(struct msghdr* msg_hdr)
+{
+	int	ttl = -1;
+	struct cmsghdr*	cmsg;
+	for (cmsg = CMSG_FIRSTHDR(msg_hdr) ; cmsg != NULL ; cmsg = CMSG_NXTHDR(msg_hdr, cmsg))
+	{
+		if (cmsg->cmsg_level == IPPROTO_IP && cmsg->cmsg_type == IP_TTL)
+			mmcpy(CMSG_DATA(cmsg), &ttl, sizeof(ttl));
+	}
+	return (ttl);
+}
+
+
